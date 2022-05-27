fc400aef66b8f51db8e02d8ae2906790adc58824
cvt666 <106361753+cvt666@users.noreply.github.com>
2022-05-26 20:03:52 -0700

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -90,8 +90,9 @@ async def start_command(client: Client, message: Message):
         reply_markup = InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("😊 About Me", callback_data = "about"),
-                    InlineKeyboardButton("🔒 Close", callback_data = "close")
+                    InlineKeyboardButton("MAIN CHANNEL",
+                    url = client.invitelink),
+                    InlineKeyboardButton("CLOSE", callback_data = "close")
                 ]
             ]
         )
